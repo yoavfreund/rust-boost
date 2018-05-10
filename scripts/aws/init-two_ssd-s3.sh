c01dd8b318793828cdb69b057f51993f915b2778
Julaiti Alafate <jalafate@gmail.com>
2018-05-10 16:53:08 +0000

    updates

--- scripts/aws/init-two_ssd-s3.sh
+++ scripts/aws/init-two_ssd-s3.sh
@@ -7,6 +7,7 @@ yes | sudo mkfs.ext4 -L MY_RAID /dev/md0
 sudo mount LABEL=MY_RAID /mnt
 sudo chown -R ubuntu /mnt
 
+sudo apt-get update
 sudo apt-get install -y awscli
 
 cd /mnt
@@ -19,7 +20,6 @@ echo "export EDITOR=vim" >> ~/.bashrc
 
 git clone $GIT_REPO /mnt/rust-boost
 
-sudo apt-get update
 yes | sudo apt-get install cargo
 
 wait 5
